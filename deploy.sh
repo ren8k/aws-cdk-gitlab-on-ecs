88f90e9e15074b2938300027a5fd364d0dc3f425
renya <rsmilek5492@gmail.com>
2024-11-22 00:59:03 +0900

    Update repository URL in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /tmp
-git clone https://github.com/ren8k/aws-cdk-ecs-gitlab.git
-cd aws-cdk-ecs-gitlab
+git clone https://github.com/ren8k/aws-cdk-gitlab-on-ecs.git
+cd aws-cdk-gitlab-on-ec
 npm ci
 npx cdk bootstrap
 npx cdk deploy
