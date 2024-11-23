ea3527976394c7061d513f9625719594d87fa446
renya <rsmilek5492@gmail.com>
2024-11-23 12:51:19 +0900

    Refactor deploy script to handle errors and improve directory navigation

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /tmp
+cd /tmp || exit
 git clone https://github.com/ren8k/aws-cdk-gitlab-on-ecs.git
-cd aws-cdk-gitlab-on-ecs
+cd aws-cdk-gitlab-on-ecs || exit
 npm ci
 npx cdk bootstrap
 npx cdk deploy
